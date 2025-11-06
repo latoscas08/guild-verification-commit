2ba806c9b921189f14d4287aa653f092c253ca7c
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:33:21 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:32:40.983Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:33:20.614Z
+Commit: Correct typo in configuration file
