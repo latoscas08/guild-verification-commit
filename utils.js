1e3197dd548ad5108c6892070bce2f7f93759fa6
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:21:12 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:20:30.346Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T01:21:11.406Z
+Commit: Fix CSS styling bug on mobile devices
