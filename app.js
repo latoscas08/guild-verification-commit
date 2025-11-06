f83a6379edbeecb616f4f8c667d5f338206bd972
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:33:10 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:32:22.871Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T01:33:08.866Z
+Commit: Resolve null pointer exception in user service
