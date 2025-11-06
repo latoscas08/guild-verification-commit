7c9a7d25e94a6f3675afcd506c358608f8f3506d
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:33:15 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:33:11.373Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T01:33:14.267Z
+Commit: Fix memory leak in event handlers
