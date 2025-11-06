15ab0b03d20d6409742ee3fb489a37cc9fdfdd65
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:32:42 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:32:16.242Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:32:40.983Z
+Commit: Fix broken links in documentation
