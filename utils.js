6a50571b05bc1dd18016600ccc19dfa678853e9c
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:18:43 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:18:17.628Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:18:42.101Z
+Commit: Add export functionality for reports
