5b65a2ab60e60a34af2621010734f3932120f19d
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:17:42 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:16:24.955Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T01:17:41.069Z
+Commit: Fix broken links in documentation
