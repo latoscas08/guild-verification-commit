26954133f891bb74dfdf6d90a1b05c86a6af85a9
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:32:36 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:32:26.914Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:32:35.313Z
+Commit: Fix memory leak in event handlers
