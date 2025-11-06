a7d849de322229d0f20f5b2f6b12a831a76f5a4a
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:17:49 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:17:44.979Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T01:17:48.791Z
+Commit: Resolve null pointer exception in user service
