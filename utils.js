07f10d0f963de8962b47e2291710c7b53043dc59
rioooos <113216389+latoscas08@users.noreply.github.com>
2025-11-06 04:26:59 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:23:57.826Z
-Commit: Add dark mode support
+Timestamp: 2025-11-06T01:26:58.205Z
+Commit: Resolve null pointer exception in user service
